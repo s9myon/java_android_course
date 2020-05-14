6d8a1e8ad43aec3bea43af520a837f8b49ba9f7b
Roman Romanov <rromanov@65apps.com>
2020-05-14 14:32:11 +0400

    Revert "Реализовал привязанный сервис для загрузки списка контактов"

--- MyApplication/app/src/main/java/com/gmail/shudss00/myapplication/ContactDetailsFragment.java
+++ MyApplication/app/src/main/java/com/gmail/shudss00/myapplication/ContactDetailsFragment.java
@@ -1,8 +1,6 @@
 package com.gmail.shudss00.myapplication;
 
-import android.content.Context;
 import android.os.Bundle;
-import android.os.IBinder;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,47 +9,25 @@ import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 
-import java.util.concurrent.ExecutionException;
-
 public class ContactDetailsFragment extends Fragment {
-    private ContactService mService;
-    private ContactService.ResultListener contactService;
 
-    static ContactDetailsFragment newInstance(int contactId, IBinder binder) {
+    static ContactDetailsFragment newInstance(int contactId) {
         ContactDetailsFragment fragment = new ContactDetailsFragment();
         Bundle args = new Bundle();
         args.putInt("contactId", contactId);
-        args.putBinder("binder", binder);
         fragment.setArguments(args);
         return fragment;
     }
-
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if(context instanceof ContactService.ResultListener) {
-            contactService = (ContactService.ResultListener) context;
-        }
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        contactService = null;
-    }
-
     //  LayoutInflater используется для установки ресурса разметки для создания интерфейса
     //  ViewGroup container устанавливает контейнер интерфейса
     //  Bundle savedInstanceState передает ранее сохраненное состояние
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         getActivity().setTitle("Контакт");
-        ContactService.ContactBinder contactBinder =
-                (ContactService.ContactBinder) getArguments().getBinder("binder");
-        mService = contactBinder.getService();
 
         View view = inflater.inflate(R.layout.contact_detail, container, false);
 
+        int contactId = this.getArguments().getInt("contactId");
         ImageView contactImage = (ImageView) view.findViewById(R.id.image_dt);
         TextView contactName = (TextView) view.findViewById(R.id.name_dt);
         TextView contactNumber = (TextView) view.findViewById(R.id.nmb_dt_1);
@@ -60,21 +36,15 @@ public class ContactDetailsFragment extends Fragment {
         TextView contactEmail2 = (TextView) view.findViewById(R.id.eml_dt_2);
         TextView contactDescr = (TextView) view.findViewById(R.id.description);
 
-        int contactId = this.getArguments().getInt("contactId");
-        Contact currentContact = null;
-        try {
-            currentContact = mService.getContactById(contactId, contactService);
-        } catch (ExecutionException | InterruptedException e) {
-            e.printStackTrace();
-        }
+        Contact curContact = Contact.contacts[contactId];
 
-        contactImage.setImageResource(currentContact.getImage());
-        contactName.setText(currentContact.getName());
-        contactNumber.setText(currentContact.getNumber());
-        contactNumber2.setText(currentContact.getNumber2());
-        contactEmail.setText(currentContact.getEmail());
-        contactEmail2.setText(currentContact.getEmail2());
-        contactDescr.setText(currentContact.getDescription());
+        contactImage.setImageResource(curContact.getImage());
+        contactName.setText(curContact.getName());
+        contactNumber.setText(curContact.getNumber());
+        contactNumber2.setText(curContact.getNumber2());
+        contactEmail.setText(curContact.getEmail());
+        contactEmail2.setText(curContact.getEmail2());
+        contactDescr.setText(curContact.getDescription());
         return view;
     }
 
--- MyApplication/app/src/main/java/com/gmail/shudss00/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/gmail/shudss00/myapplication/MainActivity.java
@@ -1,71 +1,21 @@
 package com.gmail.shudss00.myapplication;
 
-import android.content.ComponentName;
-import android.content.Context;
-import android.content.Intent;
-import android.content.ServiceConnection;
 import android.os.Bundle;
-import android.os.IBinder;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
 
-public class MainActivity extends AppCompatActivity implements ContactService.ResultListener {
-    ContactService mService;
-    ServiceConnection mConnection;
-    boolean mBound = false;
+public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(final Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mConnection = new ServiceConnection() {
-            @Override
-            public void onServiceConnected(ComponentName className, IBinder binder) {
-                // связались с ContactService, получили IBinder и экземпляр ContactService
-                ContactService.ContactBinder contactBinder = (ContactService.ContactBinder) binder;
-                mService = contactBinder.getService();
-                mBound = true;
-                if(savedInstanceState == null) {
-                    new ContactListFragment();
-                    getSupportFragmentManager().beginTransaction()
-                        .add(R.id.fragment_list, ContactListFragment.newInstance(binder))
-                        .commit();
-                }
-            }
-
-            @Override
-            public void onServiceDisconnected(ComponentName className) {
-                mBound = false;
-                mService = null;
-            }
-        };
-        // привязываемся к ContactService
-        Intent intent = new Intent(this, ContactService.class);
-        bindService(intent, mConnection, Context.BIND_AUTO_CREATE);
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        // отвязываемся от ContactService
-        if (mBound) {
-            unbindService(mConnection);
-            mBound = false;
-            mService = null;
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.fragment_list, new ContactListFragment())
+                    .commit();
         }
-    }
-
-    @Override
-    public Contact[] getContactsCallback(Contact[] contacts) {
-        return contacts;
-    }
-
-    @Override
-    public Contact getContactByIdCallback(int id, Contact[] contacts) {
-        return contacts[id];
     }
 
 }
